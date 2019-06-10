ede25f0350b169363f590dedc2bf2bc16fa4a945
Flying Serpent <nickprather11@gmail.com>
2019-06-10 18:25:01 -0400

    changed questions in html to js object, started working on display object function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 // Global vars
 //correct answer array
-var allAnswers = [3, 3, 2, 4, 4, 3, 1, 4, 4, 1, 3, 2, 1, 2, 1];
+var allAnswers = [3, 4, 2, 4, 4, 3, 1, 4, 4, 1, 3, 2, 1, 2, 1];
 //user answer array
 var userAnswers = [];
 //user correct answer num
@@ -14,6 +14,129 @@ var clockRunning = false;
 var time = 200;
 var intervalId;
 var submit = false;
+var allQuestions = [
+  {
+    question: "What is The Dude's favorite drink?",
+    choice1: "Gin and Tonic",
+    choice2: "Black Tooth Grin",
+    choice3: "White Russian",
+    choice4: "Wine Spritzer",
+    correctChoice: 3
+  },
+  {
+    question:
+      "After one of the thugs inspects his bowling ball, asking what it is, the Dude states, 'Obviously, you are not a ______.'",
+    choice1: "Bowler",
+    choice2: "Plumber",
+    choice3: "Electrician",
+    choice4: "Golfer",
+    correctChoice: 4
+  },
+  {
+    question: "What famous musician portrays one of the Nihilists?",
+    choice1: "Moby",
+    choice2: "Flea",
+    choice3: "Madonna",
+    choice4: "Cher",
+    correctChoice: 2
+  },
+  {
+    question: "What does bowling have that 'Nam does not?",
+    choice1: "Beer",
+    choice2: "Judaism",
+    choice3: "Dogs",
+    choice4: "Rules",
+    correctChoice: 4
+  },
+  {
+    question: "What band does The Dude hate?",
+    choice1: "Creedence Clearwater Revival",
+    choice2: "The Beatles",
+    choice3: "Metallica",
+    choice4: "The Eagles",
+    correctChoice: 4
+  },
+  {
+    question: "What's inside the briefcase that Walter throws to the nihilists?",
+    choice1: "Phone books",
+    choice2: "Fake money",
+    choice3: "Dirty undies",
+    choice4: "Newspaper",
+    correctChoice: 3
+  },
+  {
+    question: "'Sometimes you eat the ____, and sometimes the ____ eats you.'",
+    choice1: "Bar",
+    choice2: "Bear",
+    choice3: "World",
+    choice4: "Ball",
+    correctChoice: 1
+  },
+  {
+    question: "At the end of the movie, what drink does the mysterious stranger order?",
+    choice1: "Phosphate",
+    choice2: "Milkshake",
+    choice3: "Beer",
+    choice4: "Sarsaparilla",
+    correctChoice: 4
+  },
+  {
+    question: "What is the name of the movie The Dude hallucinates himself in?",
+    choice1: "Logjammin'",
+    choice2: "The Dude and The Dudette",
+    choice3: "The Plumber and The Viking Queen",
+    choice4: "Gutterballs",
+    correctChoice: 4
+  },
+  {
+    question: "The Big Lebowski sponsers a group of youths called the 'Little Lebowski ______ _______.'",
+    choice1: "Urban Achievers",
+    choice2: "Suburban Achievers",
+    choice3: "Rural Achievers",
+    choice4: "Urban Go-Getters",
+    correctChoice: 1
+  },
+  {
+    question: "What is the name of the Big Lebowski's butler?",
+    choice1: "Burt",
+    choice2: "Brett",
+    choice3: "Brandt",
+    choice4: "Bart",
+    correctChoice: 3
+  },
+  {
+    question: "What band's tapes did The Dude lose when his car was stolen?",
+    choice1: "The Eagles",
+    choice2: "Creedence Clearwater Revival",
+    choice3: "Bob Dylan",
+    choice4: "The Byrds",
+    correctChoice: 2
+  },
+  {
+    question: "What chain store card serves as The Dude's only form of ID?",
+    choice1: "Ralph's",
+    choice2: "Roses",
+    choice3: "Food Lion",
+    choice4: "Harris Teeter",
+    correctChoice: 1
+  },
+  {
+    question: " What kind of dog does Walter think he's babysitting for his ex?",
+    choice1: "Schnauzer",
+    choice2: "Chihuahua",
+    choice3: "Pomeranian",
+    choice4: "Labradoodle",
+    correctChoice: 3
+  },
+  {
+    question: "What is the name of the Nihilist's electro-pop band?",
+    choice1: "Autobahn",
+    choice2: "Kraftwerk",
+    choice3: "The Nihilists",
+    choice4: "Nagelbett",
+    correctChoice: 1
+  }
+];
 
 function start() {
   if (!clockRunning) {
@@ -32,8 +155,17 @@ function stop() {
   clockRunning = false;
 }
 
+function displayQuestions() {
+  for (i = 0; i < allQuestions.length; i++) {
+    var q = allQuestions[i].question;
+    var c1 = allQuestions[i].choice1;
+    var c2 = allQuestions[i].choice2;
+    var c3 = allQuestions[i].choice3;
+    var c4 = allQuestions[i].choice4;
+  }
+}
+
 // Display timer and start timer button
-$("#qBody").hide();
 $("#timer").html("<h3>" + time + " seconds left" + "</h3>");
 
 // When start button is clicked, countdown starts and questions and submit button are displayed
